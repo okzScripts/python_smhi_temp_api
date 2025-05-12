aa9cd74f88abdd385ec8b782823268154205d83f
okzScripts <oskar.krantz90@gmail.com>
2025-05-12 15:55:59 +0200

    added some error handling and prevention against too frequent db entries

--- app.py
+++ app.py
@@ -2,7 +2,25 @@
 import pandas as pd
 import numpy as np
 import sqlite3
-from datetime import datetime
+from datetime import datetime,timedelta
+
+
+def getLatestDBentry():
+    try:
+        conn = sqlite3.connect('halmstad_temp.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT time FROM prev_temps ORDER BY time DESC LIMIT 1")
+
+        last_entry = cursor.fetchone()
+
+        if last_entry:
+            return datetime.fromisoformat(last_entry[0])
+        else:
+            return None
+    except sqlite3.Error as e:
+        print(f'Error: {e}')
+    finally:
+        cursor.close()
 
 def readValues():
     df = pd.read_csv("https://opendata-download-metobs.smhi.se/api/version/1.0/parameter/1/station-set/all/period/latest-hour/data.csv",sep=';',skiprows=3)
@@ -15,23 +33,28 @@ def readValues():
 
 def WriteValueToDb(temp):
     timestamp = datetime.now().isoformat(sep=' ',timespec='seconds')
-    try:
-        conn = sqlite3.connect('halmstad_temp.db')
+    
+    if datetime.now() - getLatestDBentry() > timedelta(minutes=15):
+        try:
+            conn = sqlite3.connect('halmstad_temp.db')
 
-        cursor = conn.cursor()
+            cursor = conn.cursor()
 
-        cursor.execute('''
-            INSERT INTO prev_temps (name, celsius,time)
-            VALUES (?, ?,?)
-            ''', ('Halmstad Flygplats', temp, timestamp))
+            cursor.execute('''
+                INSERT INTO prev_temps (name, celsius,time)
+                VALUES (?, ?,?)
+                ''', ('Halmstad Flygplats', temp, timestamp))
 
-        conn.commit()
-        return 'Temperature successfully written to db'
-    except sqlite3.Error as e:
-        return f'Error writing to db: {e}'
-    finally:
-        conn.close()
-        print('Db connection closed')
+            conn.commit()
+            return 'Temperature successfully written to db'
+        except sqlite3.Error as e:
+            return f'Error writing to db: {e}'
+        finally:
+            conn.close()
+            print('Db connection closed')
+    else:
+        print(f'The latest entry was {datetime.now() - getLatestDBentry()} ago, too early to write new entry')
+        return 'Script good, but no db entry'
 
 
 print(WriteValueToDb(readValues()))
