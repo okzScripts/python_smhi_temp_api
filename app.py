f81fd3d9cd82fc83f8fcd46b41fbd1db41bbb8bf
okzScripts <oskar.krantz90@gmail.com>
2025-05-12 14:59:39 +0200

    small fixes, added more data, made a plot of historical temperatures

--- app.py
+++ app.py
@@ -1,35 +1,37 @@
-import matplotlib.pyplot as plt
+
 import pandas as pd
 import numpy as np
 import sqlite3
-import time
+from datetime import datetime
+
+def readValues():
+    df = pd.read_csv("https://opendata-download-metobs.smhi.se/api/version/1.0/parameter/1/station-set/all/period/latest-hour/data.csv",sep=';',skiprows=3)
+    df.columns = df.columns.str.strip()
 
 
-df = pd.read_csv("https://opendata-download-metobs.smhi.se/api/version/1.0/parameter/1/station-set/all/period/latest-hour/data.csv",sep=';',skiprows=3)
-df.columns = df.columns.str.strip()
+    print(df.head())
+    df_halmstad = df[df['StationsId'] == 62410 ]
+    return df_halmstad.iloc[0,5]
+
+def WriteValueToDb(temp):
+    timestamp = datetime.now().isoformat(sep=' ',timespec='seconds')
+    try:
+        conn = sqlite3.connect('halmstad_temp.db')
+
+        cursor = conn.cursor()
+
+        cursor.execute('''
+            INSERT INTO prev_temps (name, celsius,time)
+            VALUES (?, ?,?)
+            ''', ('Halmstad Flygplats', temp, timestamp))
+
+        conn.commit()
+        return 'Temperature successfully written to db'
+    except sqlite3.Error as e:
+        return f'Error writing to db: {e}'
+    finally:
+        conn.close()
+        print('Db connection closed')
 
 
-print(df.head())
-
-df_halmstad = df[df['StationsId'] == 62410 ]
-temp_halmstad = df_halmstad.iloc[0,5]
-print(df_halmstad)
-
-halmstad_string = f'Temperaturen för {df_halmstad['Stationsnamn'].values[0]} är {temp_halmstad}'
-
-current_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-
-print(halmstad_string)
-
-print(temp_halmstad)
-
-conn = sqlite3.connect('halmstad_temp.db')
-
-cursor = conn.cursor()
-
-cursor.execute('''
-    INSERT INTO prev_temps (name, celsius,time)
-    VALUES (?, ?,?)
-''', (df_halmstad['Stationsnamn'].values[0], temp_halmstad,current_time))
-
-conn.commit()
+print(WriteValueToDb(readValues()))
---
+++ client_plots.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+import sqlite3
+import pandas as pd
+import matplotlib.pyplot as plt
+
+conn = sqlite3.connect('halmstad_temp.db')
+
+cursor = conn.cursor()
+
+df = pd.read_sql_query(f'SELECT * FROM prev_temps',conn)
+
+print(df)
+
+df['time'] = pd.to_datetime(df['time'],format='mixed')
+df['time'] = df['time'].dt.floor('s')
+
+df = df.sort_values('time')
+df['celsius'] = df['celsius'].astype(float)
+
+print(df[['time','celsius']])
+
+plt.figure(figsize=(10, 5))
+plt.plot(df['time'], df['celsius'], marker='o', linestyle='-', color='skyblue')
+plt.title('Temperature - Halmstad Flygplats')
+plt.xlabel('Date')
+plt.ylabel('Temp')
+plt.grid(True)
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
